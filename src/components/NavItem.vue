12a320d7732032142c96aba81c2654aae83c85f8
asylbekuly <155750245+asylbekuly@users.noreply.github.com>
2025-04-10 10:07:41 +0500

    added the reactive variable

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="flex-1">
-    <a class="flex flex-col items-center p-2">
-        <slot> </slot>
+    <a class="flex flex-col items-center p-7" :href="$attrs.href">
+      <slot></slot>
     </a>
   </li>
 </template>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,19 +1,27 @@
 <script setup>
-import{ClockIcon,ListBulletIcon,ChartBarIcon} from '@heroicons/vue/24/outline'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/constants'
+import { ref } from 'vue'
 import NavItem from './NavItem.vue'
+
 const navItems = {
-  'timeing': ClockIcon,
-  'activities': ListBulletIcon,
-  'progress': ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon,
 }
+
+const curntPage = ref(window.location.hash.replace('#', '') || PAGE_TIMELINE)
 </script>
 
-
-
 <template>
-      <nav class="sticky bottom-0 z-10 bg-white">
+  <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around p-4 border-t">
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem 
+        v-for="(icon, page) in navItems" 
+        :key="page" 
+        :href="`#${page}`" 
+        :class="{'bg-gray-100' : page === curntPage}"  
+        @click="curntPage = page">
         <component :is="icon" class="h-6 w-6 text-gray-500" />
         {{ page }}
       </NavItem>
